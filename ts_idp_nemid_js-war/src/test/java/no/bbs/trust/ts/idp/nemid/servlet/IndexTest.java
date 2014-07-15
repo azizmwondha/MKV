093a79b9b2a040b7601d01bc783718ba6195f506
Stein Håkon Bøler <shboh@nets.eu>
2014-07-15 14:10:21 +0000

    SISB-321 - SIS: Update the NemID IDPs in E-Signing with NemID JS

--- ts_idp_nemid_js-war/src/test/java/no/bbs/trust/ts/idp/nemid/servlet/IndexTest.java
+++ ts_idp_nemid_js-war/src/test/java/no/bbs/trust/ts/idp/nemid/servlet/IndexTest.java
@@ -3,7 +3,6 @@ package no.bbs.trust.ts.idp.nemid.servlet;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.security.Security;
 
@@ -11,8 +10,6 @@ import javax.servlet.ServletException;
 
 import no.bbs.trust.common.basics.constants.Constants;
 import no.bbs.trust.common.basics.exceptions.StatusCodeException;
-import no.bbs.trust.common.basics.types.Dispatch;
-import no.bbs.trust.common.basics.types.ReturnCode;
 import no.bbs.trust.common.basics.utils.StringUtils;
 import no.bbs.trust.common.config.Config;
 import no.bbs.trust.common.config.ConfigStarter;
@@ -29,16 +26,12 @@ import org.apache.log4j.Logger;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 
 public class IndexTest {
 
 	protected static Logger logger = Logger.getLogger(Constants.MAIN_LOGGER);
 
-	private static final String SREF1 = "AE910DF8E2D812F5E53831032437F078C5F76260";
-	private static final String SREF2 = "A92ED314B462D52159965681E5FD4F3AA3AF7D28";
-	private static final String TARGET = "/index.jsp";
+	private static final String SREF = "A92ED314B462D52159965681E5FD4F3AA3AF7D28";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -48,46 +41,6 @@ public class IndexTest {
 			ConnectionFactories.getInstance().registerDBConnectionFactory(new OracleConnectionFactory());
 		}
 		Security.addProvider(new BouncyCastleProvider());
-	}
-
-	@SuppressWarnings("static-method")
-	@Test
-	public void testServiceRequest() throws ServletException {
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		request.setParameter(ConfigKeys.PARAM_SREF, SREF1);
-		MockHttpServletResponse response = new MockHttpServletResponse();
-		Index index = new Index();
-		index.init(null);
-
-		try {
-			ReturnCode returnCode = index.serviceRequest(request, response);
-			assertNotNull(returnCode);
-			assertEquals(Dispatch.INCLUDE, returnCode.getDispatch());
-			assertEquals(TARGET, returnCode.getTarget());
-			assertClientTag((String) request.getAttribute("clienttag"));
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
-	}
-
-	@SuppressWarnings("static-method")
-	@Test
-	public void testHandleRequest() throws ServletException {
-		MockHttpServletRequest request = new MockHttpServletRequest();
-		request.setParameter(ConfigKeys.PARAM_SREF, SREF1);
-		MockHttpServletResponse response = new MockHttpServletResponse();
-		Index index = new Index();
-		index.init(null);
-
-		try {
-			ReturnCode returnCode = index.handleRequest(request, response);
-			assertNotNull(returnCode);
-			assertEquals(Dispatch.INCLUDE, returnCode.getDispatch());
-			assertEquals(TARGET, returnCode.getTarget());
-			assertClientTag((String) request.getAttribute("clienttag"));
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
 	}
 
 	@SuppressWarnings("static-method")
@@ -125,7 +78,7 @@ public class IndexTest {
 	@SuppressWarnings("static-method")
 	@Test
 	public void testGenerateJsonParameters() throws StatusCodeException, ServletException {
-		final String sref = SREF2;
+		final String sref = SREF;
 		Index index = new Index();
 		index.init(null);
 		String mid = DAOUtil.getSessionDataByKey(sref, ConfigKeys.SESSIONKEY_MID);
